6b22e77e7eea568e017470c0d192bed690dd07b9
vuluu <vuluu040320@gmail.com>
2023-11-23 15:12:47 +0700

    use posgresql

--- shopee_clone/settings.py
+++ shopee_clone/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'shopee_clone.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "shopee_d91h",
+        "USER": "vuluu2k",
+        "PASSWORD": "uKqdFerRbxMZU0trLGWWIj5ps1ajxvvH",
+        "HOST": "dpg-clfg0b6g1b2c73a0s9c0-a.singapore-postgres.render.com",
+        "PORT": "5432",
     }
 }
 
