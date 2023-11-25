4aa42abdd183e42d4dec9b61b2f467e15c47dff3
vuluu2k <vuluu040320@gmail.com>
2023-11-25 16:51:20 +0700

    fix

--- categories/models.py
+++ categories/models.py
@@ -7,6 +7,7 @@ class MainCategory(models.Model):
     image = models.TextField(default="")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
