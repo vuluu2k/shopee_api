65f95be6373ffe5b954240713c731a4eb0f269d3
vuluu <vuluu040320@gmail.com>
2023-11-28 09:30:58 +0700

    add models crawler

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+
+class Crawler(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    url = models.URLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.url
+
+class Version(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    crawler = models.ForeignKey(Crawler, on_delete=models.CASCADE) 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.crawler.url + ' - ' + str(self.id) + ' - ' + str(self.created_at)
--- shopee_clone/settings.py
+++ shopee_clone/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'categories',
     'users',
     'shops',
+    'crawler',
     'rest_framework',
     'django_filters',
 ]
