2f1a88504c92c860ff6ce1a1192269f30c8bd6c5
Watermaker <vuibigboy@gmail.com>
2023-12-09 10:07:41 +0700

    fix

--- categories/urls.py
+++ categories/urls.py
@@ -1,10 +1,9 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewset, ProductByCategoryViewset,MyApiView
+from .views import CategoryViewset, ProductByCategoryViewset
 
 router = DefaultRouter()
 router.register(r'^list', CategoryViewset, basename='categories')
 router.register(r'^viewproduct', ProductByCategoryViewset, basename='categories')
-router.register(r'^myapi', MyApiView, basename='categories')
 
 urlpatterns = router.urls
