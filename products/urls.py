a7079b3399476c1d1196da5857190f18c174400a
Watermaker <vuibigboy@gmail.com>
2024-01-11 19:21:29 +0700

    add feedbacks to api

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,7 @@ from .views import ProductViewSet
 
 urlpatterns = [
     path('', ProductViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('/<str:pk>', ProductViewSet.as_view({
+    path('/<str:slug>', ProductViewSet.as_view({
         'get': 'retrieve',
         'put': 'update',
         'patch': 'partial_update',
